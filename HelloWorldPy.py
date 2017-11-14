47b672336375e84202f60936de3b96d0a9598a9c
CollinHeck <30582450+CollinHeck@users.noreply.github.com>
2017-11-14 15:27:43 -0500

    Add files via upload

--- HelloWorldPy.py
+++ HelloWorldPy.py
@@ -3,7 +3,9 @@ import cv2
 
 # Load an color image in grayscale
 img = cv2.imread('img1.jpg',1)
+#cv2.imwrite('messigray.png',img)
+#face = img[220:300, 310:360]
+#img[100:180, 100:150] = face
 cv2.imshow('image',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-#cv2.imwrite('messigray.png',img)
